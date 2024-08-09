cf6d26968206f028e043df84195d276071e1bba3
wili <wilidiribaaj@gmail.com>
2024-08-09 03:22:51 +0300

    login and logout v1

--- app.py
+++ app.py
@@ -5,6 +5,11 @@ from werkzeug.security import check_password_hash, generate_password_hash
 
 app = Flask(__name__)
 db = SQL("sqlite:///user.db")
+# Configure session to use filesystem (instead of signed cookies)
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
 
 
 @app.route("/register", methods=["GET", "POST"])
@@ -40,3 +45,54 @@ def register():
     else:
         return render_template("register.html")
 #end of reigster
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    """Log user in"""
+
+    # Forget any user_id
+    session.clear()
+
+    # User reached route via POST (as by submitting a form via POST)
+    if request.method == "POST":
+        # Ensure username was submitted
+        if not request.form.get("username"):
+            return "must provide username"
+
+        # Ensure password was submitted
+        elif not request.form.get("password"):
+            return "must provide password"
+
+        # Query database for username
+        rows = db.execute(
+            "SELECT * FROM users WHERE username = ?", request.form.get("username")
+        )
+
+        # Ensure username exists and password is correct
+        if len(rows) != 1 or not check_password_hash(
+            rows[0]["password"], request.form.get("password")
+        ):
+            return "invalid username and/or password"
+
+        # Remember which user has logged in
+        session["user_id"] = rows[0]["id"]
+
+        # Redirect user to home page
+        return redirect("/")
+
+    # User reached route via GET (as by clicking a link or via redirect)
+    else:
+        return render_template("login.html")
+
+
+@app.route("/logout")
+def logout():
+    """Log user out"""
+
+    # Forget any user_id
+    session.clear()
+
+    # Redirect user to login form
+    return redirect("/login")
+
+
+
